4402934250d2214484de879db3a560173a4ee838
axie22 <alexxie9667@gmail.com>
2024-05-08 11:39:22 -0400

    Reorganized Files

---
+++ formScript.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('input').focus(function() {
+        $(this).css('background', '#999999');
+    });
+
+    $('input').blur(function() {
+        $(this).css('background', 'white');
+    });
+
+    function toggleCustomInput() {
+        var select = document.getElementById('occupationSelect');
+        var customInput = document.getElementById('customOccupation');
+
+        if (select.value === 'other') {
+            customInput.style.display = 'block'; 
+        } else {
+            customInput.style.display = 'none'; 
+        }
+    }
+
+    function handleFormSubmit() {
+        var select = document.getElementById('occupationSelect');
+        var customInput = document.getElementById('customOccupation');
+        var occupation = select.value !== 'other' ? select.options[select.selectedIndex].text : customInput.value;
+        return occupation;
+    }
+
+    $('#contactForm').submit(function(e) {
+        e.preventDefault();
+        var name = $('#formName').val();
+        var email = $('#formEmail').val();
+        var occupation = handleFormSubmit();
+        console.log("Name: " + name);
+        console.log("Email: " + email);
+        console.log("Occupation: " + occupation);
+
+        this.reset();
+        document.getElementById('customOccupation').style.display = 'none';
+    });
+});
+
