57f6dabc4522b4a26846dcd7397c63e870ee311a
axie22 <alexxie9667@gmail.com>
2026-01-01 16:40:59 -0700

    Made it more visually appealing

---
+++ axie22/components/layout/BackgroundBlobs.tsx
@@ -0,0 +1,14 @@
+export const BackgroundBlobs = () => {
+    return (
+      <div className="fixed inset-0 -z-10 overflow-hidden pointer-events-none user-select-none">
+        {/* Top Right Blob */}
+        <div className="absolute -top-[10%] -right-[10%] w-[40rem] h-[40rem] bg-purple-500/30 rounded-full blur-3xl opacity-50 animate-pulse mix-blend-multiply dark:mix-blend-screen filter" />
+        
+        {/* Bottom Left Blob */}
+        <div className="absolute -bottom-[10%] -left-[10%] w-[40rem] h-[40rem] bg-indigo-500/30 rounded-full blur-3xl opacity-50 animate-pulse mix-blend-multiply dark:mix-blend-screen filter" style={{ animationDelay: "2s" }} />
+        
+        {/* Centerish Blob */}
+        <div className="absolute top-[40%] left-[30%] w-[30rem] h-[30rem] bg-pink-500/20 rounded-full blur-3xl opacity-30 animate-pulse mix-blend-multiply dark:mix-blend-screen filter" style={{ animationDelay: "4s" }} />
+      </div>
+    );
+  };
